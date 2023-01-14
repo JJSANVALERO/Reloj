7edc7defc52c1bb6c80c039406592fa74c871a41
JuanPe1981 <juanpe981@me.com>
2023-01-14 16:28:22 +0100

    Se para cronometro y cuenta atras

--- src/main/java/com/svalero/reloj/controller/ShowCronometroController.java
+++ src/main/java/com/svalero/reloj/controller/ShowCronometroController.java
@@ -1,8 +1,10 @@
 package com.svalero.reloj.controller;
 
 import com.svalero.reloj.tasks.CronometroTask;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.net.URL;
@@ -14,15 +16,21 @@ public class ShowCronometroController implements Initializable {
 
     @FXML
     Label lbTiempo;
+    @FXML
+    Button btParar;
 
     public ShowCronometroController(){
         this.cronometroTask = new CronometroTask();
     }
 
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.cronometroTask.messageProperty().addListener((observableValue, oldvalue, newValue) -> this.lbTiempo.setText(newValue));
         new Thread(this.cronometroTask).start();
+    }
+
+    @FXML
+    public void stop(ActionEvent event) {
+        cronometroTask.cancel();
     }
 }
--- src/main/java/com/svalero/reloj/controller/ShowWatchController.java
+++ src/main/java/com/svalero/reloj/controller/ShowWatchController.java
@@ -3,6 +3,7 @@ package com.svalero.reloj.controller;
 
 import com.svalero.reloj.tasks.ClockTask;
 import com.svalero.reloj.tasks.TimerTask;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -24,5 +25,10 @@ public class ShowWatchController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         this.timerTask.messageProperty().addListener((observableValue, oldValue, newValue) -> this.lbTiempo.setText(newValue));
         new Thread(this.timerTask).start();
+    }
+
+    @FXML
+    public void stop(ActionEvent event) {
+        timerTask.cancel();
     }
 }
